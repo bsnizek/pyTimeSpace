c37de2a89d6d4fdcd490e34197c7811bb236560d
Bernhard Snizek <bs@metascapes.org>
2014-03-13 23:04:13 +0100

    some more nifty stuff

--- pyTimeSpace/src/test/Test.py
+++ pyTimeSpace/src/test/Test.py
@@ -1,39 +1,71 @@
 '''
 Created on Mar 13, 2014
 
-@author: besn
+@author: Bernhard Snizek
 '''
 
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData, Table, Column, Integer, String
 
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 
-from geoalchemy2.types import *
+from geoalchemy2.types import Geography
 
+from sqlalchemy.ext.declarative import declarative_base
+
+import datetime
+
+Base = declarative_base()
+
+class Dot(Base):
+    __tablename__ = 'dots'
+    gid = Column(Integer, primary_key=True)
+    geography = Column(Geography(geometry_type='POINT', srid=4326))
+    time = Column('time', TIMESTAMP)
+    
+    def __init__(self, geography, time):
+        self.geography = geography
+        self.time = time
+    
+class Tracjectory(Base):
+    __tablename__ = 'trajectories'
+    gid = Column(Integer, primary_key=True)
+    geography = Column(Geography(geometry_type='POLYLINE', srid=4326))
 
 class Test:
     
     def __init__(self):
         
-        meta = MetaData()
+        self.meta = MetaData()
+        self.engine = None
+        self.dots_table = None
+        self.connection = None
         
-        dots_table = Table('dots', meta,
+    def setup(self):
+        
+        self.dots_table = Table('dots', self.meta,
                             Column('gid', Integer, primary_key=True),
                             Column('geography', Geography(geometry_type='POINT', srid=4326)),
                             Column('time', TIMESTAMP)
                             )
         
-        engine = create_engine('postgresql://abm:abm@localhost:5432/dots', echo=True)
+        self.engine = create_engine('postgresql://abm:abm@localhost:5432/dots', echo=True)
         
-        print engine
+        print self.engine
         
-        connection = engine.connect()
+        self.connection = self.engine.connect()
         
-        result = engine.execute(dots_table.select())
+        # smid nogle punkter ind 
+        
+    def add_some_points(self):
+        timestamp_one = datetime.datetime(1,2,3) 
+        
+    def test_query(self):
+        result = self.engine.execute(self.dots_table.select())
         for row in result:
             print row
 
 if __name__ == '__main__':
     test = Test();
+    test.setup()
+    test.add_some_points();
